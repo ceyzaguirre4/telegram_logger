bdac115220d06fcc4de1c0078c6551a159a9b5d4
Cristobal Eyzaguirre <ceyzaguirre4@uc.cl>
2018-05-28 11:28:43 -0400

    fixed rendering of index

--- logger_api.py
+++ logger_api.py
@@ -6,7 +6,19 @@ import json
 import datetime
 from telegram_logger import notify_subscribers, create_logger, del_logger, main
 
-app = Flask(__name__)
+
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
+
+app = CustomFlask(__name__)
 loggers = {}
 
 
